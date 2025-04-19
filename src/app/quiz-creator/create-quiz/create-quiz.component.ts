873f7750feb8274a0be88d0085e3f22cdb40b421
MMPjayarathne <pramudithajayarathna@gmail.com>
2025-04-19 23:12:34 +0530

    feat: Create quiz creation component

--- src/app/quiz-creator/create-quiz/create-quiz.component.ts
+++ src/app/quiz-creator/create-quiz/create-quiz.component.ts
@@ -1,14 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-create-quiz',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './create-quiz.component.html',
-  styleUrl: './create-quiz.component.css'
+  styleUrl: './create-quiz.component.css',
 })
 export class CreateQuizComponent {
-  
+  quizForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.quizForm = this.fb.group({
+      title: ['', Validators.required],
+      questions: this.fb.array([this.createQuestion()]),
+    });
+  }
+
+  get questions(): FormArray {
+    return this.quizForm.get('questions') as FormArray;
+  }
+
+  createQuestion(): FormGroup {
+    return this.fb.group({
+      questionText: ['', Validators.required],
+      options: this.fb.array([
+        this.fb.control('', Validators.required),
+        this.fb.control('', Validators.required),
+        this.fb.control('', Validators.required),
+        this.fb.control('', Validators.required),
+      ]),
+      correctAnswerIndex: [0, Validators.required],
+      timeLimit: [30, Validators.required],
+    });
+  }
+
+  addQuestion() {
+    this.questions.push(this.createQuestion());
+  }
+
+  submitQuiz() {
+    console.log(this.quizForm.value);
+    // Later save to Firebase or backend
+  }
+
+  getOptions(question: AbstractControl): AbstractControl[] {
+    return (question.get('options') as FormArray).controls;
+  }
+
+  removeQuestion(index: number){
+    const questions = this.quizForm.get('questions') as FormArray;
+
+    if(questions.length > 1){
+      questions.removeAt(index);
+    }
+  }
+  
 }
--- src/app/quiz-creator/quiz-creator.module.ts
+++ src/app/quiz-creator/quiz-creator.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { QuizCreatorRoutingModule } from './quiz-creator-routing.module';
+import { CreateQuizComponent } from './create-quiz/create-quiz.component';
+import { QuestionFormComponent } from './question-form/question-form.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+  ],
   imports: [
     CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
     QuizCreatorRoutingModule
   ]
 })
